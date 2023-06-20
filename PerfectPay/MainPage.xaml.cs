21c1c584cce7c715a1c1672cb969d7aa2e2c0d2b
devjuliovilla <122956628+devjuliovilla@users.noreply.github.com>
2023-06-20 13:20:43 -0600

    Creada interfaz grafica

---
+++ PerfectPay/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace PerfectPay;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void txt_bill_Completed(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void slider_tip_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+
+    }
+
+    private void btn_minus_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btn_plus_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
+
