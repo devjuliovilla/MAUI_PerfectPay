df9be31c69bcfed67dd7d38143173dff1e29b881
devjuliovilla <122956628+devjuliovilla@users.noreply.github.com>
2023-06-20 14:40:31 -0600

    Creada lógica para calcular cuenta

--- PerfectPay/MainPage.xaml.cs
+++ PerfectPay/MainPage.xaml.cs
@@ -2,6 +2,10 @@
 
 public partial class MainPage : ContentPage
 {
+    decimal bill;
+    int tip;
+    int noPersons = 1;
+
 	public MainPage()
 	{
 		InitializeComponent();
@@ -9,27 +13,55 @@ public partial class MainPage : ContentPage
 
     private void txt_bill_Completed(object sender, EventArgs e)
     {
+        bill = decimal.Parse(txt_bill.Text);
+        CalculateTotal();
+    }
 
+    private void CalculateTotal()
+    {
+        var totalTip = (bill * tip) / 100;
+        var tipByPerson = totalTip / noPersons;
+        label_tipByPerson.Text = $"{tipByPerson:C}";
+
+        var subtotal = bill / noPersons;
+        label_subtotal.Text = $"{subtotal:C}";
+
+        var totalByPerson = (bill + totalTip) / noPersons;
+        label_Total.Text = $"{totalByPerson:C}";
     }
 
     private void Button_Clicked(object sender, EventArgs e)
     {
-
+        if (sender is Button)
+        {
+            var btn = (Button)sender;
+            var percent = int.Parse(btn.Text.Replace(" %", ""));
+            slider_tip.Value = percent;
+        }
     }
 
     private void slider_tip_ValueChanged(object sender, ValueChangedEventArgs e)
     {
-
+        tip = (int)slider_tip.Value;
+        label_tip.Text = $"Tip: {tip} %";
+        CalculateTotal();
     }
 
     private void btn_minus_Clicked(object sender, EventArgs e)
     {
-
+        if (noPersons > 1)
+        {
+            noPersons--;
+            label_noPersons.Text = noPersons.ToString();
+            CalculateTotal();
+        }
     }
 
     private void btn_plus_Clicked(object sender, EventArgs e)
     {
-
+        noPersons++;
+        label_noPersons.Text = noPersons.ToString();
+        CalculateTotal();
     }
 }
 
---
+++ README.md
@@ -0,0 +1,8 @@
+# PerfectPay
+
+Esta es una aplicación desarrollada con .NET MAUI que te permite calcular el total a pagar en una cuenta. 
+
+Puedes ingresar el monto total de la cuenta, el porcentaje de propina y el número de personas entre las cuales se dividirá la cuenta. 
+
+La aplicación te proporcionará el monto a pagar por persona, teniendo en cuenta la propina.
+
